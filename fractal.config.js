769d0abd7b0a38287f10fd993e64a1301ad1ac2c
paallaire <pascalandre.allaire@gmail.com>
2023-04-28 15:21:37 -0400

    add docs

--- fractal.config.js
+++ fractal.config.js
@@ -4,13 +4,16 @@
 const fractal = module.exports = require('@frctl/fractal').create();
 
 /* Set the title of the project */
-fractal.set('project.title', 'FooCorp Component Library');
+fractal.set('project.title', 'Styleguide Base');
 
 /* Tell Fractal where the components will live */
 fractal.components.set('path', __dirname + '/styleguide/components');
 
 /* Tell Fractal where the documentation pages will live */
 fractal.docs.set('path', __dirname + '/styleguide/docs');
+
+/* destination for the static export */
+fractal.web.set('builder.dest', 'docs'); 
 
 const mandelbrot = require('@frctl/mandelbrot');
 
