4d8b1d6d7336ee72b5e4275e134e1cff416d67e1
paallaire <pascalandre.allaire@gmail.com>
2023-04-28 16:45:21 -0400

    Test twig

--- fractal.config.js
+++ fractal.config.js
@@ -3,6 +3,11 @@
 /* Create a new Fractal instance and export it for use elsewhere if required */
 const fractal = module.exports = require('@frctl/fractal').create();
 
+/* loading twig */
+const twigAdapter = require('@frctl/twig')();
+fractal.components.engine(twigAdapter);
+fractal.components.set('ext', '.twig');
+
 /* Set the title of the project */
 fractal.set('project.title', 'Styleguide Base');
 
@@ -15,11 +20,18 @@ fractal.docs.set('path', __dirname + '/styleguide/docs');
 /* destination for the static export */
 fractal.web.set('builder.dest', 'docs'); 
 
+/* Hard-code a port number to start the server on. */
+fractal.web.set('server.port', 4000);
+
+/* Specify a directory of static assets */
+//fractal.web.set('static.path', `${__dirname}/public/dist`);
+
+
 const mandelbrot = require('@frctl/mandelbrot');
 
 const myCustomisedTheme = mandelbrot({
-    skin: "fuchsia",
-    panels: ["html", "info", "resources"]
+    skin: "black",
+    panels: ['html', 'view', 'context', 'resources', 'info', 'notes']
 });
 
 // myCustomisedTheme.addLoadPath(__dirname + '/styleguide/_theme-overrides');
